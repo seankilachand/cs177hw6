077ace33c54b51852cf1d59e24b4c331d909e438
Sean Kilachand <seank@brandeis.edu>
2011-05-02 22:26:23 -0400

    hw6 script

---
+++ hw6.m
@@ -0,0 +1,10 @@
+test_scoresIncreasing();
+test_cigarParty();
+test_bunnyEars();
+test_close10();
+test_sortaSum();
+test_firstlast6();
+test_hasTeen();
+test_lastDigit();
+test_parrot_trouble();
+test_sumDouble();
